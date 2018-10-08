29ed514650777b877c9d9af18e2ca730c96dec40
Alexandre <alexandreferreirass@hotmail.com>
2018-10-08 10:22:26 -0300

    Criando aula 0810 / Variaveis globais e local

---
+++ Aulas/Aula0810/Aula0810.cpp
@@ -0,0 +1,29 @@
+// Aula0810.cpp : Defines the entry point for the console application.
+//
+
+/*Incluir comentario com 
+mais de uma linha 
+*/
+
+#include "stdafx.h"
+//C ANSI
+#include <stdio.h> //Stander I/O biblicoteca padrão de entrada e saída de dados (perifericos)
+#include <stdlib.h>
+
+//variaveis
+
+int a;//inteiro
+float b; //flutuante
+double c; // flutuante (maior precisão, mais casas decimais)
+bool d; //booleano (verdadeiro/falso-->true/falso)
+
+
+
+int main()
+{
+
+	int e; //variavel local 
+	
+    return 0;
+}
+
