e383bef347e12e79438a65466d55a0abb9d66df0
Alexandre <alexandreferreirass@hotmail.com>
2018-10-10 08:58:50 -0300

    New Project Aula1010

---
+++ Aulas/Aula1010_Funcoes/Aula1010_Funcoes.cpp
@@ -0,0 +1,12 @@
+// Aula1010_Funcoes.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <stdlb.h>
+
+int main()
+{
+    return 0;
+}
+
