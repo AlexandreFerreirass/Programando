698257c170846f75d143d77cdf9fbde32414f0b1
Alexandre <alexandreferreirass@hotmail.com>
2018-10-17 09:20:22 -0300

    Calculadora simples

--- Aulas/Aula1010_Funcoes/Aula1010_Funcoes.cpp
+++ Aulas/Aula1010_Funcoes/Aula1010_Funcoes.cpp
@@ -1,12 +1,39 @@
 // Aula1010_Funcoes.cpp : Defines the entry point for the console application.
-//
+//Set as StartUp Project---> definir o projeto a ser executado
+//F5 executa o debug
+//F9 break points  - executa e para na linha definida
+//F10 executa sem mostrar (passo-a-passo)
+//F11 executa (passo-a-passo)
+// valores truncados 
 
 #include "stdafx.h"
+#include <stdlib.h>
 #include <stdio.h>
-#include <stdlb.h>
+
+void HelloWord()// void-->Função não possui retorno
+{
+	printf("Hello World!\n");
+}
+
+
+
+int somade2valores(int a, int b)
+{
+	int c;
+	c = a + b;
+	return c;
+}
 
 int main()
 {
-    return 0;
+	HelloWord(); //Telecense autocomplete--> funções são identificadas por um cubo 
+	int d=somade2valores(10, 10);
+	printf("Digite os dois valores: %i\n");
+	
+
+	printf("\Resultado da soma: %i\n", d);
+	system("pause");
+	return 0;
+		
 }
 
---
+++ Aulas/Aula1010calculadora/Aula1010calculadora.cpp
@@ -0,0 +1,53 @@
+// Aula1010calculadora.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include<locale.h>
+
+
+
+
+float soma(float a, float b)
+{
+	float cs = a + b;
+	return cs;
+}
+
+float multiplicacao(float a, float b)
+{
+	float cm = a * b;
+	return cm;
+}
+
+float divisao(float a, float b)
+{
+	float cd = a / b;
+	return cd;
+}
+float subtracao(float a, float b)
+{
+	float cs = a - b;
+	return cs;
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "portuguese");
+	float a, b;
+
+	printf_s("Digite o primeiro valor: ");
+	scanf_s("%f", &a);
+	printf_s("Digite o segundo valor: ");
+	scanf_s("%f", &b);
+		
+	printf_s("O valor da soma é: %f\n", soma(a,b));
+	printf_s("O valor da multiplicação é: %f\n", multiplicacao(a, b));
+	printf_s("O valor da divisão é: %f\n", divisao(a, b));
+	printf_s("O valor da subtração é: %f\n", subtracao(a, b));
+
+	system("pause");
+}
+
